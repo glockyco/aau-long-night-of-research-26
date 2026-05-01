d5e4cd5d09e06cc3cc482ac23cba25e0dfdd41e4
Johann Glock <11704293+glockyco@users.noreply.github.com>
2026-05-01 11:08:09 +0200

    feat(data): add team members with zod schema

---
+++ src/lib/data/team.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const teamMemberSchema = z.object({
+  name: z.string(),
+  role: z.object({
+    de: z.string(),
+    en: z.string()
+  }),
+  url: z.string().url()
+});
+
+export const teamSchema = z.array(teamMemberSchema);
+
+export type TeamMember = z.infer<typeof teamMemberSchema>;
+
+export const team: TeamMember[] = [
+  {
+    name: 'Martin Pinzger',
+    role: { de: 'Betreuer', en: 'Supervisor' },
+    url: 'https://pinzger.github.io/'
+  },
+  {
+    name: 'Johann Glock',
+    role: { de: 'ich', en: 'myself' },
+    url: 'https://glockyco.com/'
+  },
+  {
+    name: 'Clemens Bauer',
+    role: { de: 'Kollege', en: 'colleague' },
+    url: 'https://www.aau.at/en/team/bauer-clemens/'
+  }
+];
